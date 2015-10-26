c8336a606d1fedac7496ce8c25026cd65e8d9b5c
Armon <armon.shariati@gmail.com>
2015-10-26 18:59:43 -0400

    finished ec

--- haar2D.m
+++ haar2D.m
@@ -5,16 +5,20 @@
 % - outputs: C
 %       C : two-dimensional haar coefficients
 %=======================================================
-function C = haar2D(A)
+function C = haar2D(A, k)
+
+if nargin < 2
+    k = log2(size(A, 1));
+end
 
 B = [];
 for i=1:size(A, 1)
-    B = [B; haar(A(i, 1:end))];
+    B = [B; haar(A(i, 1:end), k)];
 end
 
 C = [];
 for i=1:size(A, 1)
-    C = [C, haar(B(1:end, i)')'];
+    C = [C, haar(B(1:end, i), k')'];
 end
 
 return;
--- haar_inv2D.m
+++ haar_inv2D.m
@@ -5,16 +5,20 @@
 % - outputs: A
 %       A : original two-dimensional matrix
 %=======================================================
-function A = haar_inv2D(C)
+function A = haar_inv2D(C, k)
+
+if nargin < 2
+    k = log2(size(A, 1));
+end
 
 B = [];
 for i=1:size(C, 1)
-    B = [B, haar_inv(C(1:end, i)')'];
+    B = [B, haar_inv(C(1:end, i)', k)'];
 end
 
 A = [];
 for i=1:size(C, 1)
-    A = [A; haar_inv(B(i, 1:end))];
+    A = [A; haar_inv(B(i, 1:end), k)];
 end
 
 return;
